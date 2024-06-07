2020623fca31ab5d5b61c94e095fb43ff8f22700
Seaotter6382 <hueyquigley@gmail.com>
2024-06-07 10:39:37 -0400

    addded the bet money thing

--- code.py
+++ code.py
@@ -1,11 +1,21 @@
 import time
 import random
-import math
-import sys
 
 money = 0
+start_money = 10
 
-print("how much money do you want to start with?")
-word = input().lower()
-money = int(word)
-print("your money is now " + str(money))
+money = money + start_money
+print("you have " + str(money) + " money")
+time.sleep(0.5)
+while True:
+    print("how much money do you want to bet?")
+    try:
+        word = int(input())
+    except ValueError as e:
+        print("enter a number not a letter")
+        continue
+    if word > money:
+        print("enter a number less than your money")
+    if word <= money:
+        print(word)
+        break
